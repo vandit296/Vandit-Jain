774678f1facae77c7b2c351ff85a265f35b51f27
Vandit Jain <vandit296@gmail.com>
2024-09-08 16:55:00 +0530

    Updates

---
+++ static/js/952.75fc2d0f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgame=self.webpackChunkgame||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.75fc2d0f.chunk.js.map
